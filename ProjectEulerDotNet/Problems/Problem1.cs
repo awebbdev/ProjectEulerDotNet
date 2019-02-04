11c19cf84733e79916f44253fa5551a6c879caa9
Adam Webb <awebb@cjcweb.com>
2019-02-04 11:03:57 -0600

    Removed Unit Test to be readded later

---
+++ ProjectEulerDotNet/Problems/Problem1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEulerDotNet.Problems
+{
+    class Problem1
+    {
+        public int ThreeAndFiveMult (int limit)
+        {
+            throw new NotImplementedException("Please create test first");
+        }
+    }
+}
