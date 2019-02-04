08156b165afa0630159b837ff7ca98be975531f3
Adam Webb <awebb@cjcweb.com>
2019-02-04 10:28:52 -0600

    Add project files.

---
+++ ProjectEulerDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectEulerDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
