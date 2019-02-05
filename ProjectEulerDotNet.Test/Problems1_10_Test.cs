edfd0435b2a7f999a9f61cd6eeab5a53ae3d1a8c
Adam Webb <awebb@cjcweb.com>
2019-02-05 15:58:38 -0600

    Switched to xUnit tests

---
+++ ProjectEulerDotNet.Test/Problems1_10_Test.cs
@@ -0,0 +1,47 @@
+using System;
+using Xunit;
+using ProjectEulerDotNet.Problems;
+
+namespace ProjectEulerDotNet.Test
+{
+    public class Problems1_10_Test
+    {
+        Problems1_10 _p1_10 = new Problems1_10();
+
+        [Fact]
+        public void Problem1()
+        {
+            Assert.Equal(23, _p1_10.Problem1(10));
+        }
+        [Fact]
+        public void Problem2()
+        {
+            Assert.Equal(44, _p1_10.Problem2(100));
+        }
+        [Fact]
+        public void Problem3()
+        {
+            Assert.Equal(29, _p1_10.Problem3(13195));
+        }
+        [Fact]
+        public void Problem4()
+        {
+            Assert.Equal(9009, _p1_10.Problem4(2));
+        }
+        [Fact]
+        public void Problem5()
+        {
+            Assert.Equal(2520, _p1_10.Problem5(10));
+        }
+        [Fact]
+        public void Problem6()
+        {
+            Assert.Equal(2640, _p1_10.Problem6(10));
+        }
+        [Fact]
+        public void Problem7()
+        {
+            Assert.Equal(13, _p1_10.Problem7(6));
+        }
+    }
+}
